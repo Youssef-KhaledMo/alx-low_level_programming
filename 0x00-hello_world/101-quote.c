90cb7fc75561824e30dbae9e669a199d362eca34
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 01:40:28 +0300

    setup 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unidio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *	write(int fd,const void *buf,size_t count)
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
