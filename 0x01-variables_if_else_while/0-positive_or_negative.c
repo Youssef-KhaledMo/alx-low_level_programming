0f8f3e1c80b3efdae756d9e99815d1703826973c
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 02:36:55 +0300

    setup 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validetion
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > O)
+		printf("%i is positive\n", n);
+	else if (n 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n)
+	return (O);
+}
