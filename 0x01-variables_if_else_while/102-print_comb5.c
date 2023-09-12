0cfa623014ad658bda0408493a38411a46596de6
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 04:26:16 +0300

    add new file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,7 +26,7 @@ int main(void)
 				putchar((digit2 / 10) + 48);
 				putchar((digit2 % 10) + 48);
 
-				if (digit != 98 || digit2 != 99)
+				if (digit1 != 98 || digit2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
