0593c52c681729ee4b53a9284209885b20b18e11
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-13 00:04:17 +0300

    add new file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,53 +10,15 @@
  * Return: void
 */
 
-void print_number(int n);
-
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		for (; n < 98; n++)
-		{
-			if (n != 98)
-			{
-				print_number(n);
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else
-	{
-		for (; n > 98; n--)
-		{
-			if (n != 98)
-			{
-				print_number(n);
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	print_number(98);
-	_putchar('\n');
-}
+	int counter;
 
-void print_number(int n)
-{
-	if (n >= 10)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		_putchar((n / -10) + '0');
-		_putchar((n % 10) * -1 + '0');
-	}
+	if (n > 98)
+		for (counter = n; counter > 98; counter--)
+			printf("%d, ", counter);
 	else
-	{
-		_putchar(n + '0');
-	}
+		for (counter = n; counter < 98; counter++)
+			printf("%d, ", counter);
+	printf("98\n");
 }
