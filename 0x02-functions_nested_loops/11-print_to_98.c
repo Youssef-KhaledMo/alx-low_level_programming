aa4bca2bcc692137218716d9a6d37ff0eb470375
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 23:38:58 +0300

    add new file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,53 @@
  * Return: void
 */
 
+void print_number(int n);
+
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
 		for (; n < 98; n++)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				print_number(n);
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else
 	{
 		for (; n > 98; n--)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				print_number(n);
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
-	_putchar('9');
-	_putchar('8');
+	print_number(98);
 	_putchar('\n');
+}
+
+void print_number(int n)
+{
+	if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar((n / -10) + '0');
+		_putchar((n % 10) * -1 + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
 }
