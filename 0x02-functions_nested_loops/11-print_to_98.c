24f58ded49f91e54a3a88553252bf088a0fcce9a
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 23:30:01 +0300

    add new file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,27 @@
 
 void print_to_98(int n)
 {
-	for (; n < 98; n++)
+	if (n <= 98)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (; n < 98; n++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (; n > 98; n--)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	_putchar('9');
 	_putchar('8');
+	_putchar('\n');
 }
