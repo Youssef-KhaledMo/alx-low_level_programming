73558e126177c632ec2d393bf1cadc8b02e730e5
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 17:30:05 +0300

    add new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all characters ten times
+ *
+ * Return: void.
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
