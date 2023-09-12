ee485d74185386209d074bf0332260a75dcc6ae1
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 17:25:08 +0300

    add new file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - print all characters using _putchar
  *
- * Return: Always 0.
+ * Return: void.
 */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }
