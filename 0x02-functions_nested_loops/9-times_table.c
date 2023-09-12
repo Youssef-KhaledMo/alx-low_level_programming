e8a5dd55567908f11b068b4ec40df2607f3ef2e4
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 22:34:41 +0300

    add new file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,20 +10,29 @@
 
 void times_table(void)
 {
-	int n1, n2;
+	int n1, n2, num;
 
 	for (n1 = 0; n1 <= 9; n1++)
 	{
 		for (n2  = 0; n2 <= 9; n2++)
 		{
-			if (n2 > 0)
+			if (n1 > 0)
 			{
 				_putchar(' ');
-				if (n1 > 0 && n1 < 10)
+				if ((n1 * n2) < 10)
+				{
 					_putchar(' ');
+					_putchar((n1 * n2) + '0');
+				}
+				else
+				{
+					num = (n1 * n2) / 10;
+					_putchar(num + '0');
+					_putchar(((n1 * n2) % 10) + '0');
+				}
 			}
-			_putchar((n1 * n2) + '0');
-			_putchar(',');
+			if (n2 < 9)
+				_putchar(',');
 		}
 		_putchar('\n');
 	}
