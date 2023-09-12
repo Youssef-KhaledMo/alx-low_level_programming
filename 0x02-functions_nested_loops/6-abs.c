d5f29a4c4970119ad2f2974660092c9af6bf5c09
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 21:04:22 +0300

    add new file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the abselute
+ * @n: The integer
+ *
+ * Description: calculate the abselute
+ *
+ * Return: the absulte of integer n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
