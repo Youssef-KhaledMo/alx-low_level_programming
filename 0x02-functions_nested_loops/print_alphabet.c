4d564e107396d0fb25d9e1b268d05894921dff9d
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 17:18:07 +0300

    add new file

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print all characters using _putchar
+ *
+ * Return: void.
+*/
+
+void print_alphabet(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		_putchar(str[i];
+	_putchar('\n');
+}
