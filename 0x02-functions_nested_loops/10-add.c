67abd09e7e483090f10c37fa1959aff5e50583db
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-12 22:42:16 +0300

    add new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: first integer
+ * @n2: secound integer
+ *
+ * Description: adds two integers and returns the result.
+ *
+ * Return: the summetion
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
