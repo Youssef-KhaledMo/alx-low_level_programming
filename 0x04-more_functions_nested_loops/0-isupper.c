80a62c970d2c4c3074623da3facbae9b08c4a0c1
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-14 23:28:56 +0300

    add new file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  *
  * @c: input for alphabet
  *
- * Return: 1 if its uppercase, O if not
+ * Return: 1 if its uppercase, 0 if not
 */
 
 int _isupper(int c)
@@ -13,5 +13,5 @@ int _isupper(int c)
 	if (c >= 65 && c <= 90)
 		return (1);
 	else
-		return (O);
+		return (0);
 }
