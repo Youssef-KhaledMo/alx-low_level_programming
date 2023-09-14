7510ba0aeddbdcbb309856f63d71a16e34a7c010
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-14 23:38:01 +0300

    add new file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies to integer
+ *
+ * @a: first integer
+ * @b: secound integer
+ *
+ * Return: result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
