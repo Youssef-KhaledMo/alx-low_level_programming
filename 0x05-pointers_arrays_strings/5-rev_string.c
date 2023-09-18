f55db211e7a90e57f304a9031ec25728c534fb4f
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-18 18:41:11 +0300

    add new file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,7 @@ void rev_string(char *s)
 	for (len = 0; s[len] != '\0'; len++)
 		;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		s[i] += s[len - i - 1];
 		s[len - i - 1] = s[i] - s[len - i - 1];
