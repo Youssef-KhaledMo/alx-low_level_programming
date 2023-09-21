82147bca4d059ee44ea2aac91e7d1f3f7f56639d
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2023-09-21 21:07:59 +0300

    add new file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - make all string uppercase.
+ * @s: the string.
+ *
+ * Return: string
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for 9i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+
+	return (s);
+}
